f1bde10f013784b6910e416f462ba7bf8407f891
root <root@maca25400702875.example.com>
2014-11-18 18:21:41 -0500

    replaced reconfiguration of /etc/libvirt/libvirtd.conf with define
    Signed-off-by: root <root@maca25400702875.example.com>

--- live_migration/libvirt/manifests/configure.pp
+++ live_migration/libvirt/manifests/configure.pp
@@ -1,19 +1,44 @@
 class libvirt::configure {
   $libvirt_file="/etc/sysconfig/libvirtd"
   $libvirt_conf="/etc/libvirt/libvirtd.conf"
+  define change_line ($line,$match,$path) {
+    file_line {$title:
+      require => File[$path], 
+      path => $path,
+      line => $line,
+      match => $match,
+    }
+  }
   file {$libvirt_conf:
     require => Class['libvirt::package'],
     ensure => present,
   }
-  file_line {$libvirt_conf:
-    require => File[$libvirt_conf],
-    path => $libvirt_conf,
+  change_line {'a':
     line =>'listen_tls = 0',
     match => "^.*listen_tls =.*$",
+    path => $libvirt_conf,
+    before => File[$libvirt_file], 
+  }
+  change_line {'b':
+    line =>'listen_tcp = 1',
+    match => "^.*listen_tcp =.*$",
+    path => $libvirt_conf,
+    before => File[$libvirt_file], 
+  }
+  change_line {'c':
+    line =>'auth_tcp = "none"',
+    match => "^.*auth_tcp =.*$",
+    path => $libvirt_conf,
+    before => File[$libvirt_file], 
+  }
+  change_line {'d':
+    line =>'auth_tls = "none"',
+    match => "^.*auth_tls =.*$",
+    path => $libvirt_conf,
+    before => File[$libvirt_file], 
   }
   file {$libvirt_file:
     ensure => present,
-    require => File_line[$libvirt_conf],
   }
   file_line {$libvirt_file:
     require => File[$libvirt_file],
